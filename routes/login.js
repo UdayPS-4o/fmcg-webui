bccdf95495cb01c18557976d9afe2b6977864f8b
Uday PS <contact@udayps.com>
2024-04-02 22:28:41 +0530

    godown 70%

--- routes/login.js
+++ routes/login.js
@@ -5,37 +5,37 @@ const path = require("path");
 const {redirect} = require("./utilities");
 
 
-app.post("/api/login", async (req, res) => {
-    const formData = req.body;
-    console.log(formData);
-    const { username, password } = formData;
-    const filePath = path.join(__dirname, "..", "db", "users.json");
-    try {
-      let dbData = await fs
-        .readFile(filePath, "utf8")
-        .then((data) => JSON.parse(data))
-        .catch(() => {
-          throw new Error("Database file read error or file does not exist.");
-        });
-  
-      const user = dbData.find(
-        (entry) => entry.username === username && entry.password === password
-      );
-  
-      if (user) {
-        const newToken = Math.random().toString(36).substring(7);
-        user.token = newToken;
-        await fs.writeFile(filePath, JSON.stringify(dbData, null, 2), "utf8");
-        res.status(200)
-          .header("Set-Cookie", `token=${newToken}; Path=/; Max-Age=3600;`)
-          .send("Login successful." + redirect(`/db/cash-receipts`, 500))
-      } else {
-        res.status(404).send("Error: Invalid username or password.");
-      }
-    } catch (err) {
-      console.error(err);
-      res.status(500).send("Failed to login." + err);
+app.post("/login", async (req, res) => {
+  const formData = req.body;
+  console.log(formData);
+  const { username, password } = formData;
+  const filePath = path.join(__dirname, "..", "db", "users.json");
+  try {
+    let dbData = await fs
+      .readFile(filePath, "utf8")
+      .then((data) => JSON.parse(data))
+      .catch(() => {
+        throw new Error("Database file read error or file does not exist.");
+      });
+
+    const user = dbData.find(
+      (entry) => entry.username === username && entry.password === password
+    );
+
+    if (user) {
+      const newToken = Math.random().toString(36).substring(7);
+      user.token = newToken;
+      await fs.writeFile(filePath, JSON.stringify(dbData, null, 2), "utf8");
+      res.status(200)
+        .header("Set-Cookie", `token=${newToken}; Path=/; Max-Age=3600;`)
+        .send("Login successful." + redirect(`/db/cash-receipts`, 500))
+    } else {
+      res.status(404).send("Error: Invalid username or password.");
     }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Failed to login." + err);
+  }
 });
   
 app.get("/login", (req, res) => {
